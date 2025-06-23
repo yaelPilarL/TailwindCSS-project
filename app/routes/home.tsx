1e7ad3033070f2e6b9ebf86adafffe535d6d118f
Yael Pilar <yaelpilarluque@gmail.com>
2025-06-23 13:20:12 +0000

    feat(@theme): color, title, body, elevation, icon size

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,50 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+export default function () {
+  return (
+    <>
+      <div className="mb-3 mt-3 ">
+        <div className="mb-3 flex w-[2115px] h-[350px] ">
+          <h1 className="bg-primary-1 w-[235px] h-[350px] ">1</h1>
+          <h1 className="bg-primary-5 w-[235px] h-[350px]">5</h1>
+          <h1 className="bg-primary-10 w-[235px] h-[350px]">10</h1>
+          <h1 className="bg-primary-20 w-[235px] h-[350px]  ">20</h1>
+          <h1 className="bg-primary-30 w-[235px] h-[350px]  ">30</h1>
+          <h1 className="bg-primary-40 text-white w-[235px] h-[350px] ">40</h1>
+          <h1 className="bg-primary-50 text-white w-[235px] h-[350px] ">50</h1>
+          <h1 className="bg-primary-60 text-white w-[235px] h-[350px] ">60</h1>
+          <h1 className="bg-primary-70 text-white w-[235px] h-[350px] ">70</h1>
+        </div>
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+        <div className="mb-3 ">
+          <h1 className="bg-neutral-0">0</h1>
+          <h1 className="bg-neutral-5">5</h1>
+          <h1 className="bg-neutral-10">10</h1>
+          <h1 className="bg-neutral-20">20</h1>
+          <h1 className="bg-neutral-30">30</h1>
+          <h1 className="bg-neutral-40">40</h1>
+          <h1 className="bg-neutral-50 text-white">50</h1>
+          <h1 className="bg-neutral-60 text-white">60</h1>
+          <h1 className="bg-neutral-70 text-white">70</h1>
+        </div>
+        <div className="mb-3">
+          <h1 className="bg-functional-poisitive">func/positive </h1>
+          <h1 className="bg-functional-danger"> func/danger </h1>
+        </div>
+        <div className="mb-3 ">
+          <h1 className="bg-user-orange-40">orange/40</h1>
+          <h1 className="bg-user-purple-40">purple/40</h1>
+          <h1 className="bg-user-blue-30">blue/30</h1>
+          <h1 className="bg-user-blue-40"> blue/40 </h1>
+          <h1 className="bg-user-blue-50 text-white">blue/50 </h1>
+          <h1 className="bg-user-green-40">green/40</h1>
+          <h1 className="bg-user-green-50">green/50</h1>
+        </div>
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
-}
-
-export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+        <div className="mb-3 ">
+          <h1 className="bg-userCompliments-yellow-40 "> yellow/40 </h1>
+          <h1 className="bg-userCompliments-green-30"> green/30</h1>
+          <h1 className="bg-userCompliments-gold-40"> gold/40 </h1>
+        </div>
+      </div>
+    </>
+  );
 }
