be54493f24be217fd2385fed1bc12121d6919db0
Yael Pilar <yaelpilarluque@gmail.com>
2025-06-25 19:42:42 +0000

    feat(home.tsx): colors

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,75 +1,125 @@
 export default function () {
   return (
     <>
-      <div className="mb-9 mt-3">
-        <div className="mb- flex w-[2115px] h-[350px] text-title-xlarge font-bold  ">
-          <h1 className="bg-primary-1 w-[235px] h-[350px]  ">1</h1>
-          <h1 className="bg-primary-5 w-[235px] h-[350px] ">5</h1>
-          <h1 className="bg-primary-10 w-[235px] h-[350px]  ">10</h1>
-          <h1 className="bg-primary-20 w-[235px] h-[350px]  ">20</h1>
-          <h1 className="bg-primary-30 w-[235px] h-[350px] ">30</h1>
-          <h1 className="bg-primary-40 text-white w-[235px] h-[350px] ">40</h1>
-          <h1 className="bg-primary-50 text-white w-[235px] h-[350px] ">50</h1>
-          <h1 className="bg-primary-60 text-white w-[235px] h-[350px] ">60</h1>
-          <h1 className="bg-primary-70 text-white w-[235px] h-[350px] ">70</h1>
-        </div>
-
-        <div className="mb-3 font-family">
-          <div className="mb-3 flex w-[2115px] h-[350px] font-bold text-title-xlarge">
-            <h1 className="bg-neutral-0 w-[235px] h-[350px] ">0</h1>
-            <h1 className="bg-neutral-5 w-[235px] h-[350px] ">5</h1>
-            <h1 className="bg-neutral-10 w-[235px] h-[350px] ">10</h1>
-            <h1 className="bg-neutral-20 w-[235px] h-[350px] ">20</h1>
-            <h1 className="bg-neutral-30 w-[235px] h-[350px] ">30</h1>
-            <h1 className="bg-neutral-40 w-[235px] h-[350px] ">40</h1>
-            <h1 className="bg-neutral-50 text-white w-[235px] h-[350px] ">
+      <div className="mt-[114px] ml-[181px] pr-[246px]">
+        <h1 className="bg-primary-20 font-bold inline-block text-[96px]">
+          colors
+        </h1>
+        <div className="font-family text-title-xlarge font-bold mb-[117px]">
+          <h1 className="mb-[30px]">primary</h1>
+          <div className="mb-3 flex w-[2115px] h-[350px] text-title-xlarge font-bold border border-black rounded-[8px] overflow-hidden ">
+            <h1 className="bg-primary-1 w-[235px] h-[350px] pl-[24px] pt-[24px]">
+              1
+            </h1>
+            <h1 className="bg-primary-5 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
+              5
+            </h1>
+            <h1 className="bg-primary-10 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
+              10
+            </h1>
+            <h1 className="bg-primary-20 w-[235px] h-[350px]  pl-[24px] pt-[24px]">
+              20
+            </h1>
+            <h1 className="bg-primary-30 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
+              30
+            </h1>
+            <h1 className="bg-primary-40 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
+              40
+            </h1>
+            <h1 className="bg-primary-50 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
               50
             </h1>
-            <h1 className="bg-neutral-60 text-white w-[235px] h-[350px] ">
+            <h1 className="bg-primary-60 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
               60
             </h1>
-            <h1 className="bg-neutral-70 text-white w-[235px] h-[350px] ">
+            <h1 className="bg-primary-70 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
               70
             </h1>
           </div>
         </div>
-        <div className="mb-3">
-          <div className="mb-3 flex w-[644px] h-[350px] font-bold text-title-xlarge">
-            <h1 className="bg-functional-poisitive w-[235px] h-[350px] ">
+
+        <div className="mb-[117px] font-family text-title-xlarge font-bold">
+          <h1 className="mb-[30px]">neutral</h1>
+          <div className="mb-3 flex w-[2115px] h-[350px] border border-black rounded-[8px] overflow-hidden">
+            <h1 className="bg-neutral-0 w-[235px] h-[350px] pl-[24px] pt-[24px]">
+              0
+            </h1>
+            <h1 className="bg-neutral-5 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
+              5
+            </h1>
+            <h1 className="bg-neutral-10 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
+              10
+            </h1>
+            <h1 className="bg-neutral-20 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
+              20
+            </h1>
+            <h1 className="bg-neutral-30 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
+              30
+            </h1>
+            <h1 className="bg-neutral-40 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
+              40
+            </h1>
+            <h1 className="bg-neutral-50 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
+              50
+            </h1>
+            <h1 className="bg-neutral-60 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
+              60
+            </h1>
+            <h1 className="bg-neutral-70 text-white w-[235px] h-[350px] pl-[24px] pt-[24px] ">
+              70
+            </h1>
+          </div>
+        </div>
+
+        <div className="mb-[117px] font-family text-title-xlarge font-bold">
+          <h1 className="mb-[30px]">functional</h1>
+          <div className="mb-3 flex w-[644px] h-[350px] border border-black rounded-[8px] overflow-hidden">
+            <h1 className="bg-functional-poisitive w-[322px] h-[350px] pl-[24px] pt-[24px] ">
               func/positive
             </h1>
-            <h1 className="bg-functional-danger w-[235px] h-[350px] ">
+            <h1 className="bg-functional-danger w-[322px] h-[350px] pl-[24px] pt-[24px]">
               func/danger
             </h1>
           </div>
         </div>
-        <div className="mb-3 ">
-          <div className="mb-3 flex w-[1645px] h-[350px] font-bold text-title-xlarge">
-            <h1 className="bg-user-orange-40 w-[235px] h-[350px] ">
+
+        <div className="mb-[117px] font-family text-title-xlarge font-bold">
+          <h1 className="mb-[30px]">user</h1>
+          <div className="mb-3 flex w-[1645px] h-[350px] border border-black rounded-[8px] overflow-hidden">
+            <h1 className="bg-user-orange-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
               orange/40
             </h1>
-            <h1 className="bg-user-purple-40 w-[235px] h-[350px] ">
+            <h1 className="bg-user-purple-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
               purple/40
             </h1>
-            <h1 className="bg-user-blue-30 w-[235px] h-[350px] ">blue/30</h1>
-            <h1 className="bg-user-blue-40 w-[235px] h-[350px] ">blue/40</h1>
-            <h1 className="bg-user-blue-50 text-white w-[235px] h-[350px] ">
+            <h1 className="bg-user-blue-30 w-[235px] h-[350px] pl-[24px] pt-[24px]">
+              blue/30
+            </h1>
+            <h1 className="bg-user-blue-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
+              blue/40
+            </h1>
+            <h1 className="bg-user-blue-50 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
               blue/50
             </h1>
-            <h1 className="bg-user-green-40 w-[235px] h-[350px] ">green/40</h1>
-            <h1 className="bg-user-green-50 w-[235px] h-[350px] ">green/50</h1>
+            <h1 className="bg-user-green-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
+              green/40
+            </h1>
+            <h1 className="bg-user-green-50 w-[235px] h-[350px] pl-[24px] pt-[24px]">
+              green/50
+            </h1>
           </div>
         </div>
 
-        <div className="mb-3 ">
-          <div className="mb-3 flex w-[705px] h-[350px] font-bold">
-            <h1 className="bg-userCompliments-yellow-40 w-[235px] h-[350px] ">
+        <div className="mb-[117px] font-family text-title-xlarge font-bold">
+          <h1 className="mb-[30px]">userCompliments</h1>
+          <div className="mb-3 flex w-[705px] h-[350px] border border-black rounded-[8px] overflow-hidden">
+            <h1 className="bg-userCompliments-yellow-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
               yellow/40
             </h1>
-            <h1 className="bg-userCompliments-green-30 w-[235px] h-[350px] ">
+            <h1 className="bg-userCompliments-green-30 w-[235px] h-[350px] pl-[24px] pt-[24px]">
               green/30
             </h1>
-            <h1 className="bg-userCompliments-gold-40 w-[235px] h-[350px] ">
+            <h1 className="bg-userCompliments-gold-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
               gold/40
             </h1>
           </div>
