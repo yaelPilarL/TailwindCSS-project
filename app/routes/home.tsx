5681bfbaeae36ad9babdae690dcde5dd5b6de6da
Yael Pilar <yaelpilarluque@gmail.com>
2025-06-27 19:11:11 +0000

    feat(Button) button component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,127 +1,80 @@
+import { Button } from "~/Button";
+
 export default function () {
   return (
     <>
       <div className="mt-[114px] ml-[181px] pr-[246px]">
-        <h1 className="bg-primary-20 font-bold inline-block text-[96px]">
-          colors
-        </h1>
-        <div className="font-family text-title-xlarge font-bold mb-[117px]">
-          <h1 className="mb-[30px]">primary</h1>
-          <div className="mb-3 flex w-[2115px] h-[350px] text-title-xlarge font-bold border border-black rounded-[8px] overflow-hidden ">
-            <h1 className="bg-primary-1 w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              1
-            </h1>
-            <h1 className="bg-primary-5 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
-              5
-            </h1>
-            <h1 className="bg-primary-10 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
-              10
-            </h1>
-            <h1 className="bg-primary-20 w-[235px] h-[350px]  pl-[24px] pt-[24px]">
-              20
-            </h1>
-            <h1 className="bg-primary-30 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
-              30
-            </h1>
-            <h1 className="bg-primary-40 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              40
-            </h1>
-            <h1 className="bg-primary-50 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              50
-            </h1>
-            <h1 className="bg-primary-60 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              60
-            </h1>
-            <h1 className="bg-primary-70 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              70
-            </h1>
-          </div>
-        </div>
+        <div>
+          <h1 className="bg-primary-20 font-bold inline-block text-[40px] mb-[50px]">
+            button
+          </h1>
+          <div className="flex items-start gap-[64px]">
+            <div className="w-2/3 space-y-[36px]">
+              <div>
+                <h2 className="text-neutral-50 mb-[18px] mt-[50px]">primary</h2>
+                <div className="flex gap-[36px]">
+                  <Button variant="primary">button</Button>
+                  <Button variant="primary" state="hover">
+                    button
+                  </Button>
+                  <Button variant="primary" state="focused">
+                    button
+                  </Button>
+                  <Button variant="primary" state="pressed">
+                    button
+                  </Button>
+                  <Button variant="primary" state="disabled">
+                    button
+                  </Button>
+                </div>
 
-        <div className="mb-[117px] font-family text-title-xlarge font-bold">
-          <h1 className="mb-[30px]">neutral</h1>
-          <div className="mb-3 flex w-[2115px] h-[350px] border border-black rounded-[8px] overflow-hidden">
-            <h1 className="bg-neutral-0 w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              0
-            </h1>
-            <h1 className="bg-neutral-5 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
-              5
-            </h1>
-            <h1 className="bg-neutral-10 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
-              10
-            </h1>
-            <h1 className="bg-neutral-20 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
-              20
-            </h1>
-            <h1 className="bg-neutral-30 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
-              30
-            </h1>
-            <h1 className="bg-neutral-40 w-[235px] h-[350px] pl-[24px] pt-[24px] ">
-              40
-            </h1>
-            <h1 className="bg-neutral-50 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              50
-            </h1>
-            <h1 className="bg-neutral-60 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              60
-            </h1>
-            <h1 className="bg-neutral-70 text-white w-[235px] h-[350px] pl-[24px] pt-[24px] ">
-              70
-            </h1>
-          </div>
-        </div>
+                <h2 className="text-neutral-50 mb-[18px] mt-[50px]">
+                  secondary
+                </h2>
+                <div className="flex gap-[36px]">
+                  <Button variant="secondary">button</Button>
+                  <Button variant="secondary" state="hover">
+                    button
+                  </Button>
+                  <Button variant="secondary" state="focused">
+                    button
+                  </Button>
+                  <Button variant="secondary" state="pressed">
+                    button
+                  </Button>
+                  <Button variant="secondary" state="disabled">
+                    button
+                  </Button>
+                </div>
 
-        <div className="mb-[117px] font-family text-title-xlarge font-bold">
-          <h1 className="mb-[30px]">functional</h1>
-          <div className="mb-3 flex w-[644px] h-[350px] border border-black rounded-[8px] overflow-hidden">
-            <h1 className="bg-functional-poisitive w-[322px] h-[350px] pl-[24px] pt-[24px] ">
-              func/positive
-            </h1>
-            <h1 className="bg-functional-danger w-[322px] h-[350px] pl-[24px] pt-[24px]">
-              func/danger
-            </h1>
-          </div>
-        </div>
+                <h2 className="text-neutral-50 mb-[18px] mt-[50px]">
+                  tertiary
+                </h2>
 
-        <div className="mb-[117px] font-family text-title-xlarge font-bold">
-          <h1 className="mb-[30px]">user</h1>
-          <div className="mb-3 flex w-[1645px] h-[350px] border border-black rounded-[8px] overflow-hidden">
-            <h1 className="bg-user-orange-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              orange/40
-            </h1>
-            <h1 className="bg-user-purple-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              purple/40
-            </h1>
-            <h1 className="bg-user-blue-30 w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              blue/30
-            </h1>
-            <h1 className="bg-user-blue-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              blue/40
-            </h1>
-            <h1 className="bg-user-blue-50 text-white w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              blue/50
-            </h1>
-            <h1 className="bg-user-green-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              green/40
-            </h1>
-            <h1 className="bg-user-green-50 w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              green/50
-            </h1>
-          </div>
-        </div>
-
-        <div className="mb-[117px] font-family text-title-xlarge font-bold">
-          <h1 className="mb-[30px]">userCompliments</h1>
-          <div className="mb-3 flex w-[705px] h-[350px] border border-black rounded-[8px] overflow-hidden">
-            <h1 className="bg-userCompliments-yellow-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              yellow/40
-            </h1>
-            <h1 className="bg-userCompliments-green-30 w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              green/30
-            </h1>
-            <h1 className="bg-userCompliments-gold-40 w-[235px] h-[350px] pl-[24px] pt-[24px]">
-              gold/40
-            </h1>
+                <div className="flex gap-[36px]">
+                  <Button variant="tertiary">button</Button>
+                  <Button variant="tertiary" state="hover">
+                    button
+                  </Button>
+                  <Button variant="tertiary" state="focused">
+                    button
+                  </Button>
+                  <Button variant="tertiary" state="pressed">
+                    button
+                  </Button>
+                  <Button variant="tertiary" state="disabled">
+                    button
+                  </Button>
+                </div>
+                <div className="flex gap-[100px] text-neutral-50 mt-[30px] mb-[30px] text-title-medium ">
+                  <p>default</p>
+                  <p>hover</p>
+                  <p>focused</p>
+                  <p>pressed</p>
+                  <p>disabled</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
