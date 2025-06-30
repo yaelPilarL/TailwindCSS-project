2278e0ac9f7280329bff56b826730fcdb4103263
Yael Pilar <yaelpilarluque@gmail.com>
2025-06-30 14:09:07 +0000

    fix(Button.tsx): simpler logic

--- app/Button.tsx
+++ app/Button.tsx
@@ -11,73 +11,55 @@ type ButtonProps = {
   state?: ButtonState;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
+const baseStyles =
+  "flex items-center justify-center px-[32px] py-[12px] rounded-[8px] w-[122px] h-[48px] outline-offset-2 font-medium text-body-medium/[24px] text-neutral-70";
+
+const variantBase = {
+  primary: "bg-primary-10 text-neutral-70 border-neutral-70 border-2",
+  secondary: "bg-neutral-0 text-neutral-70 border-neutral-70 border-2",
+  tertiary: "bg-transparent border-2 border-transparent",
+};
+
+const stateStyles = {
+  primary: {
+    default: "shadow-elevation-4",
+    hover: "bg-primary-5 shadow-elevation-4",
+    focused: "bg-primary-5 shadow-elevation-4 border-4",
+    pressed: "bg-primary-20",
+    disabled: "bg-neutral-60 text-neutral-40",
+  },
+  secondary: {
+    default: "shadow-elevation-4",
+    hover: "bg-neutral-20 shadow-elevation-4",
+    focused: "bg-neutral-20 shadow-elevation-4 border-4",
+    pressed: "bg-neutral-40",
+    disabled: "bg-neutral-60 text-neutral-40",
+  },
+  tertiary: {
+    default: "",
+    hover: "bg-neutral-20 shadow-elevation-4 border-neutral-70 border-2",
+    focused: "bg-neutral-20 shadow-elevation-4 border-neutral-70 border-4",
+    pressed: "bg-neutral-40 border-neutral-70 border-2",
+    disabled: "bg-neutral-60 text-neutral-40 border-neutral-70 border-2",
+  },
+};
+
 export function Button({
   children = "button",
-  variant,
+  variant = "primary",
   className,
   state = "default",
   ...buttonProps
 }: ButtonProps) {
-  const baseStyles =
-    "flex items-center justify-center px-[32px] py-[12px] rounded-[8px] w-[122px] h-[48px] outline-offset-2 font-medium text-body-medium/[24px] text-neutral-70";
-
-  let variantStyles = "";
-
-  switch (variant) {
-    case "primary":
-      variantStyles = twMerge(
-        "bg-primary-10 text-neutral-70 outline-neutral-70 border-neutral-70 border-2",
-        state === "default" ? "shadow-elevation-4" : "",
-        state === "hover" ? "bg-primary-5 shadow-elevation-4" : "",
-        state === "focused"
-          ? " bg-primary-5 shadow-elevation-4 text-neutral-70 outline-neutral-70 border-neutral-70 border-4"
-          : "",
-        state === "pressed" ? "bg-primary-20" : "",
-        state === "disabled"
-          ? "bg-neutral-60 text-neutral-40  outline-neutral-70 border-neutral-70 border-2"
-          : "",
-      );
-      break;
-
-    case "secondary":
-      variantStyles = twMerge(
-        "bg-neutral-0 text-neutral-70 outline-neutral-70 border-neutral-70 border-2",
-        state === "default" ? "shadow-elevation-4" : "",
-        state === "hover" ? "bg-neutral-20 shadow-elevation-4" : "",
-        state === "focused"
-          ? "bg-neutral-20 shadow-elevation-4 text-neutral-70 outline-neutral-70 border-neutral-70 border-4"
-          : "",
-        state === "pressed" ? "bg-neutral-40" : "",
-        state === "disabled"
-          ? "bg-neutral-60 text-neutral-40  outline-neutral-70 border-neutral-70 border-2"
-          : "",
-      );
-      break;
-
-    case "tertiary":
-      variantStyles = twMerge(
-        "bg-transparent border-2 border-transparent",
-        state === "hover"
-          ? "bg-neutral-20 shadow-elevation-4 text-neutral-70 outline-neutral-70 border-neutral-70 border-2"
-          : "",
-        state === "focused"
-          ? "bg-neutral-20 shadow-elevation-4 text-neutral-70 outline-neutral-70 border-neutral-70 border-4"
-          : "",
-        state === "pressed"
-          ? "bg-neutral-40 text-neutral-70 outline-neutral-70 border-neutral-70 border-2"
-          : "",
-        state === "disabled"
-          ? "bg-neutral-60 text-neutral-40  outline-neutral-70 border-neutral-70 border-2"
-          : "",
-      );
-      break;
-  }
+  const styles = twMerge(
+    baseStyles,
+    variantBase[variant],
+    stateStyles[variant][state],
+    className,
+  );
 
   return (
-    <button
-      className={twMerge(baseStyles, variantStyles, className)}
-      {...buttonProps}
-    >
+    <button className={styles} {...buttonProps}>
       {children}
     </button>
   );
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -21,76 +21,72 @@ export default function () {
             </p>
           </div>
 
-          <hr className="border-b-4 border-neutral-70 mt-5  "></hr>
+          <hr className="border-b-4 border-neutral-70 mt-5"></hr>
 
-          <div className="flex items-start gap-[64px]">
-            <div className="w-2/3 space-y-[36px]">
-              <div>
-                <h2 className="text-neutral-50 mb-[18px] mt-[50px] text-title-medium">
-                  primary
-                </h2>
-                <div className="flex gap-[36px]">
-                  <Button variant="primary">button</Button>
-                  <Button variant="primary" state="hover">
-                    button
-                  </Button>
-                  <Button variant="primary" state="focused">
-                    button
-                  </Button>
-                  <Button variant="primary" state="pressed">
-                    button
-                  </Button>
-                  <Button variant="primary" state="disabled">
-                    button
-                  </Button>
-                </div>
+          <div>
+            <h2 className="text-neutral-50 mb-[18px] mt-[50px] text-title-medium">
+              primary
+            </h2>
+            <div className="flex gap-[36px]">
+              <Button variant="primary">button</Button>
+              <Button variant="primary" state="hover">
+                button
+              </Button>
+              <Button variant="primary" state="focused">
+                button
+              </Button>
+              <Button variant="primary" state="pressed">
+                button
+              </Button>
+              <Button variant="primary" state="disabled">
+                button
+              </Button>
+            </div>
 
-                <h2 className="text-neutral-50 mb-[18px] mt-[50px] text-title-medium">
-                  secondary
-                </h2>
-                <div className="flex gap-[36px]">
-                  <Button variant="secondary">button</Button>
-                  <Button variant="secondary" state="hover">
-                    button
-                  </Button>
-                  <Button variant="secondary" state="focused">
-                    button
-                  </Button>
-                  <Button variant="secondary" state="pressed">
-                    button
-                  </Button>
-                  <Button variant="secondary" state="disabled">
-                    button
-                  </Button>
-                </div>
+            <h2 className="text-neutral-50 mb-[18px] mt-[50px] text-title-medium">
+              secondary
+            </h2>
+            <div className="flex gap-[36px]">
+              <Button variant="secondary">button</Button>
+              <Button variant="secondary" state="hover">
+                button
+              </Button>
+              <Button variant="secondary" state="focused">
+                button
+              </Button>
+              <Button variant="secondary" state="pressed">
+                button
+              </Button>
+              <Button variant="secondary" state="disabled">
+                button
+              </Button>
+            </div>
 
-                <h2 className="text-neutral-50 mb-[18px] mt-[50px] text-title-medium">
-                  tertiary
-                </h2>
+            <h2 className="text-neutral-50 mb-[18px] mt-[50px] text-title-medium">
+              tertiary
+            </h2>
 
-                <div className="flex gap-[36px]">
-                  <Button variant="tertiary">button</Button>
-                  <Button variant="tertiary" state="hover">
-                    button
-                  </Button>
-                  <Button variant="tertiary" state="focused">
-                    button
-                  </Button>
-                  <Button variant="tertiary" state="pressed">
-                    button
-                  </Button>
-                  <Button variant="tertiary" state="disabled">
-                    button
-                  </Button>
-                </div>
-                <div className="flex gap-[100px] text-neutral-50 mt-[30px] mb-[30px] text-title-medium ">
-                  <p>default</p>
-                  <p>hover</p>
-                  <p>focused</p>
-                  <p>pressed</p>
-                  <p>disabled</p>
-                </div>
-              </div>
+            <div className="flex gap-[36px]">
+              <Button variant="tertiary">button</Button>
+              <Button variant="tertiary" state="hover">
+                button
+              </Button>
+              <Button variant="tertiary" state="focused">
+                button
+              </Button>
+              <Button variant="tertiary" state="pressed">
+                button
+              </Button>
+              <Button variant="tertiary" state="disabled">
+                button
+              </Button>
+            </div>
+            <div className="flex gap-[100px] text-neutral-50 mt-[30px] mb-[30px] text-title-medium ">
+              <p>default</p>
+              <p>hover</p>
+              <p>focused</p>
+              <p>pressed</p>
+              <p>disabled</p>
             </div>
           </div>
         </div>
