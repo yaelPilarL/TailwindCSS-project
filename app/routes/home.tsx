acd427e4e03d1d8442c9185ced417ff5ba2bd80d
Yael Pilar <yaelpilarluque@gmail.com>
2025-07-01 19:06:48 +0000

    feat(card component): landscape and portrait

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,8 @@
 import { faBell as Bell } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "~/Button";
+import { Portrait } from "~/Portrait";
+import { Landscape } from "~/Landscape";
 
 export default function () {
   return (
@@ -30,38 +32,22 @@ export default function () {
               primary
             </h2>
             <div className="flex gap-[36px]">
-              <Button variant="primary">button</Button>
-              <Button variant="primary" state="hover">
-                button
-              </Button>
-              <Button variant="primary" state="focused">
-                button
-              </Button>
-              <Button variant="primary" state="pressed">
-                button
-              </Button>
-              <Button variant="primary" state="disabled">
-                button
-              </Button>
+              <Button variant="primary"></Button>
+              <Button variant="primary" state="hover"></Button>
+              <Button variant="primary" state="focused"></Button>
+              <Button variant="primary" state="pressed"></Button>
+              <Button variant="primary" state="disabled"></Button>
             </div>
 
             <h2 className="text-neutral-50 mb-[18px] mt-[50px] text-title-medium">
               secondary
             </h2>
             <div className="flex gap-[36px]">
-              <Button variant="secondary">button</Button>
-              <Button variant="secondary" state="hover">
-                button
-              </Button>
-              <Button variant="secondary" state="focused">
-                button
-              </Button>
-              <Button variant="secondary" state="pressed">
-                button
-              </Button>
-              <Button variant="secondary" state="disabled">
-                button
-              </Button>
+              <Button variant="secondary"></Button>
+              <Button variant="secondary" state="hover"></Button>
+              <Button variant="secondary" state="focused"></Button>
+              <Button variant="secondary" state="pressed"></Button>
+              <Button variant="secondary" state="disabled"></Button>
             </div>
 
             <h2 className="text-neutral-50 mb-[18px] mt-[50px] text-title-medium">
@@ -69,19 +55,11 @@ export default function () {
             </h2>
 
             <div className="flex gap-[36px]">
-              <Button variant="tertiary">button</Button>
-              <Button variant="tertiary" state="hover">
-                button
-              </Button>
-              <Button variant="tertiary" state="focused">
-                button
-              </Button>
-              <Button variant="tertiary" state="pressed">
-                button
-              </Button>
-              <Button variant="tertiary" state="disabled">
-                button
-              </Button>
+              <Button variant="tertiary"></Button>
+              <Button variant="tertiary" state="hover"></Button>
+              <Button variant="tertiary" state="focused"></Button>
+              <Button variant="tertiary" state="pressed"></Button>
+              <Button variant="tertiary" state="disabled"></Button>
             </div>
             <div className="flex gap-[100px] text-neutral-50 mt-[30px] mb-[30px] text-title-medium ">
               <p>default</p>
@@ -100,7 +78,7 @@ export default function () {
             Provides access to notifications and informs users of severity.
           </p>
           <hr className="border-b-4 border-neutral-70 mt-5"></hr>
-          <div className="my-7">
+          <div className="my-7 mb-24">
             <div className="flex items-center justify-center h-[62px] w-[62px] rounded-[8px] bg-neutral-10 border-neutral-70 border-2">
               <div className="relative">
                 <FontAwesomeIcon
@@ -108,6 +86,27 @@ export default function () {
                   className="w-[var(--icon-size-24)] h-[var(--icon-size-24)]"
                 />
                 <div className="absolute left-[16px] top-[-2px] w-[10px] h-[10px] rounded-full bg-primary-20 "></div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h1 className="bg-primary-20 font-bold inline-block text-[40px] mb-[40px]">
+              actionCard
+            </h1>
+            <p className="text-neutral-60 ">Card element that is actionable.</p>
+            <hr className="border-b-4 border-neutral-70 mt-5 mb-7"></hr>
+            <div className="flex flex-col items-center  ">
+              <div className="flex gap-[164px] mb-[81px] ml-[221px]">
+                <Portrait></Portrait>
+                <Portrait state="hover"></Portrait>
+                <Portrait state="focused"></Portrait>
+                <Portrait state="pressed"></Portrait>
+              </div>
+              <div className=" flex gap-[24px] ml-[162px]">
+                <Landscape></Landscape>
+                <Landscape state="hover"></Landscape>
+                <Landscape state="focused"></Landscape>
+                <Landscape state="pressed"></Landscape>
               </div>
             </div>
           </div>
