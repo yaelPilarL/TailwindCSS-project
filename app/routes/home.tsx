0db2684b9aab957a1ae52ac71e789a2959ea9153
Yael Pilar <yaelpilarluque@gmail.com>
2025-07-01 13:35:26 +0000

    feat(FontAwesomeIcon): notification bell

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,12 @@
+import { faBell as Bell } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "~/Button";
 
 export default function () {
   return (
     <>
       <div className="mt-[50px] ml-[181px] pr-[246px]">
-        <div>
+        <div className="mb-24">
           <h1 className="bg-primary-20 font-bold inline-block text-[40px] mb-[40px]">
             button
           </h1>
@@ -87,6 +89,26 @@ export default function () {
               <p>focused</p>
               <p>pressed</p>
               <p>disabled</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1 className="bg-primary-20 font-bold inline-block text-[40px] mb-[40px]">
+            notificationBell
+          </h1>
+          <p className="text-neutral-60 ">
+            Provides access to notifications and informs users of severity.
+          </p>
+          <hr className="border-b-4 border-neutral-70 mt-5"></hr>
+          <div className="my-7">
+            <div className="flex items-center justify-center h-[62px] w-[62px] rounded-[8px] bg-neutral-10 border-neutral-70 border-2">
+              <div className="relative">
+                <FontAwesomeIcon
+                  icon={Bell}
+                  className="w-[var(--icon-size-24)] h-[var(--icon-size-24)]"
+                />
+                <div className="absolute left-[16px] top-[-2px] w-[10px] h-[10px] rounded-full bg-primary-20 "></div>
+              </div>
             </div>
           </div>
         </div>
